0e5eafedc967cc4dfd253de3b24d5e8602e8658c
Герасименко Ярослав <gerasimenkoooo37@gmail.com>
2025-08-26 03:04:02 +0300

    json get from pydabtic model

--- resources/routers/tasks.py
+++ resources/routers/tasks.py
@@ -1,14 +1,20 @@
-from fastapi import APIRouter, Depends
+
+from fastapi import APIRouter
+from pydantic import BaseModel
 from core.di import command_bus, container
 from domain.commands.tasks import SendPingCommand
-from resources.dependencies import get_session_id
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
+
+# Pydantic-модель для входящего JSON
+class PingRequest(BaseModel):
+    session_id: str
+
 @router.post("/ping")
-async def send_ping(session_id: str = Depends(get_session_id)):
+async def send_ping(data: PingRequest):
     # создаём команду и исполняем её через шину с адаптером из контейнера
     queue_port = container.resolve("task_queue")
-    cmd = SendPingCommand(session_id=session_id)
+    cmd = SendPingCommand(session_id=data.session_id)
     task_id = cmd.execute(bus=queue_port)
     return {"data": {"task_id": task_id}}
